370374f0f79b435f51697f0f476339b86629102f
Soo <soojlee0106@naver.com>
2023-04-04 22:31:13 +0900

    dynamic programming question

--- baekjoon/2751.py
+++ baekjoon/2751.py
@@ -1,5 +1,6 @@
 import sys
 input = sys.stdin.readline
+# 백줌 문제의 경우 input() 대신 sys.stdin.readline()을 사용해야 시간초과가 나지 않는다.
 
 numbers = []
 
@@ -7,4 +8,4 @@ for i in range(int(input())):
     numbers.append(int(input()))
 
 for i in sorted(numbers):
-    print(i)
+    print(i)  # 한 줄에 하나씩 출력
