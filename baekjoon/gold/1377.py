8b8fd4c6caf16be08ef6a16b25b92fe1a80ad770
Soo <soojlee0106@naver.com>
2023-04-07 00:52:57 +0900

    time problem

--- baekjoon/gold/1377.py
+++ baekjoon/gold/1377.py
@@ -1,15 +1,16 @@
 import sys
-N = int(sys.stdin.readline())
-A = [0] + [int(sys.stdin.readline())
-           for _ in range(N)] + [1000000]  # 0 is added for convenience
+input = sys.stdin.readline
 
-changed = False
-for i in range(1, N+2):  # N+2 is the last index to be compared
-    changed = False
-    for j in range(1, N-i+1):  # N-i+1 is the last index to be compared
-        if A[j] > A[j+1]:
-            changed = True
-            A[j], A[j+1] = A[j+1], A[j]
-    if not changed:  # if no change is made, the list is sorted
-        print(i)
-        break
+n = int(input())
+arr = []
+for i in range(n):
+    arr.append((int(input()), i))  # (값, 인덱스) 튜플로 저장
+
+# 둘을 비교하기 위해 정렬
+sorted_arr = sorted(arr)
+answer = []
+
+for i in range(n):
+    answer.append(sorted_arr[i][1] - arr[i][1])  # 정렬된 배열의 인덱스 - 원래 배열의 인덱스
+
+print(max(answer) + 1)  # 인덱스는 0부터 시작하므로 +1
